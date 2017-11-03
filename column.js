dce80d465ab24f47c350eb2a64a780d3b50060e1
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-11-03 12:52:59 +0100

    Add files via upload

--- column.js
+++ column.js
@@ -2,7 +2,7 @@ function Column(id, name) {
 	var self = this;
 	
 	this.id = id;
-	this.name = name;
+	this.name = name || 'Nie podano nazwy';
 	this.element = createColumn();
 
 	function createColumn() {
@@ -15,29 +15,25 @@ function Column(id, name) {
 		
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
 		columnDelete.click(function() {
-    var self = this;
-    $.ajax({
-      url: baseUrl + '/column/' + self.id,
-      method: 'DELETE',
-      success: function(response){
-        self.element.remove();
-      }
-    });})
+			self.deleteColumn();
+		});
 		
 		columnAddCard.click(function(event) {
-	var cardName = prompt("Enter the name of the card");
-	event.preventDefault();
-	$.ajax({
-        url: baseUrl + '/card',
-        method: 'POST',
-        data: {
-              //body query
-        },
-        success: function() {
-            //create a new client side card
-        }
-    });
-});
+			var cardName = prompt("Wpisz nazwę karty");
+			event.preventDefault();
+			$.ajax({
+			    url: baseUrl + '/card',
+			    method: 'POST',
+			    data: {
+				    name: cardName,
+				    bootcamp_kanban_column_id: self.id
+			    },
+			    success: function(response) {
+			        var card = new Card(response.id, cardName);
+			        self.createCard(card);
+			    }
+			});
+		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
 		column.append(columnTitle)
